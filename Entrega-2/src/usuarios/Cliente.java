74d66d3ae8c7eaf1b25b49335d5ec5c48dca8b40
carolinaceballosg <c.ceballosg@uniandes.edu.co>
2026-04-09 17:17:06 -0500

    clases basicas paquete usuario

---
+++ Entrega-2/src/usuarios/Cliente.java
@@ -0,0 +1,50 @@
+package clasesparte1;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente extends Usuario {
+	private int puntosFidelidad;
+	private List<JuegoDeMesa> juegosFavoritos;
+	
+	public Cliente(String login, String password, int puntosFidelidad) {
+		super(login, password);
+		this.puntosFidelidad = puntosFidelidad;
+        this.juegosFavoritos = new ArrayList<>();
+	}
+	
+	 public int getPuntosFidelidad(){
+	        return puntosFidelidad;
+	    }
+	    public void setPuntosFidelidad(int puntosFidelidad){
+	        this.puntosFidelidad = puntosFidelidad;
+	    }
+	    public List<JuegoDeMesa> getJuegosFavoritos(){
+	        return juegosFavoritos;
+	    }
+	    public void setJuegosFavoritos(List<JuegoDeMesa> juegosFavoritos){
+	        this.juegosFavoritos = juegosFavoritos;
+	    }
+	    public void agregarPuntos(double nuevo) {
+	    	int puntosGanados = (int)(nuevo*0.01);
+	    	this.puntosFidelidad= this.puntosFidelidad + puntosGanados;
+	    }
+	    public void usarPuntos(int cant) {
+	    	if (cant <= 0) {
+	    		System.out.println("Cantidad inválida");
+	            return;
+	        }
+	        if (cant > this.puntosFidelidad) {
+	        	System.out.println("Puntos insuficientes");
+	            return;
+	        }
+	        this.puntosFidelidad = this.puntosFidelidad - cant;
+	    }
+	    public void agregarJuegoFav(JuegoDeMesa fav) {
+	    	if (fav == null) {
+	            return;
+	        }
+	        if (!this.juegosFavoritos.contains(fav)) {
+	            this.juegosFavoritos.add(fav);
+	        }
+	    }
+}
